b3b2a1db9f2e9fb88d51f8d30b5f091e2a813ecc
Lisa Leung <110@holbertonschool.com>
2017-09-30 22:24:16 +0000

    copied 3 into 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,56 +1,95 @@
-$(document).ready(function(){
+const $ = window.$;
+$(document).ready(function () {
   let amenityDictionary = {};
+
   $.ajax({
-     url: 'http://0.0.0.0:5001/api/v1/places_search/',
-     type: 'post',
-     contentType: 'application/json',
-     data: JSON.stringify({}),
-     success: function(data ){
-       for (let i = 0; i < data.length; i++) {
-	 let article = $('<article>');
-         article.append("div", data[i].price_by_night);
-         $(".places").append(article);
-       }
-     },
-     error: function( jqXhr, textStatus, errorThrown ){
-       console.log(errorThrown);
-     }
-  });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status == "OK") {
-      $("DIV#api_status").addClass("available");
-    } else {
-      $("DIV#api_status").removeClass("available");
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let article = $('<article>');
+        /* TITLE */
+        let title = $('<div/>', {class: 'title'}).appendTo(article); /* create title */
+        $('<h2/>', {text: data[i]['name']}).appendTo(title); /* append to title */
+        $('<div/>', {class: 'price_by_night', text: '$' + data[i].price_by_night}).appendTo(title);
+
+        /* INFORMATION */
+        let infoContainer = $('<div/>', {class: 'information'}).appendTo(article);
+        let maxGuest = $('<div/>', {class: 'max_guest'}).appendTo(infoContainer);
+        let numberRoom = $('<div/>', {class: 'number_rooms'}).appendTo(infoContainer);
+        let numberBath = $('<div/>', {class: 'number_bathrooms'}).appendTo(infoContainer);
+
+        /* GUEST ICON */
+        $('<i/>', {class: 'fa fa-users fa-3x'}).appendTo(maxGuest);
+        $('<br/>').appendTo(maxGuest);
+        $('<div/>', {text: data[i]['max_guest'] + ' Guests'}).appendTo(maxGuest);
+
+        /* NUMBER OF ROOMS */
+        $('<i/>', {class: 'fa fa-bed fa-3x'}).appendTo(numberRoom);
+        $('<br/>').appendTo(numberRoom);
+        $('<div/>', {text: data[i]['number_rooms'] + ' Bedrooms'}).appendTo(numberRoom);
+
+        /* NUMBER OF BATHS */
+        $('<i/>', {class: 'fa fa-bath fa-3x'}).appendTo(numberBath);
+        $('<br/>').appendTo(numberBath);
+        $('<div/>', {text: data[i]['number_bathrooms'] + ' Bathroom'}).appendTo(numberBath);
+
+        /* USER */
+        let user = $('<div/>', {class: 'user'});
+
+        /* Separate API call to populate user information */
+        $.ajax({
+          url: 'http://0.0.0.0:5001/api/v1/users/' + data[i]['user_id'],
+          type: 'get',
+          success: function (userData) {
+            $('<strong/>', {text: 'Owner: ' + userData['first_name'] + ' ' + userData['last_name']}).appendTo(user);
+            user.appendTo(article);
+            $('<div/>', {class: 'description', text: data[i]['description']}).appendTo(user);
+          }
+        });
+        $('.places').append(article);
+      }
+    },
+    error: function (jqXhr, textStatus, errorThrown) {
+      console.log(errorThrown);
     }
   });
-  $("input").change(function(){
-    let result = "";
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+  $('input').change(function () {
+    let result = '';
     let i = 0;
-    if(this.checked){
-      amenityDictionary[$(this).attr("data-id")] = $(this).attr("data-name");
+    if (this.checked) {
+      amenityDictionary[$(this).attr('data-id')] = $(this).attr('data-name');
       for (let key in amenityDictionary) {
         if (i !== 0) {
-          result += ", " + amenityDictionary[key];
+          result += ', ' + amenityDictionary[key];
         } else {
           result += amenityDictionary[key];
         }
         i += 1;
       }
-      $(".amenities h4").text(result);
-
-    }else {
-      let result = "";
+      $('.amenities h4').text(result);
+    } else {
+      let result = '';
       let i = 0;
-      delete (amenityDictionary[$(this).attr("data-id")]);
+      delete (amenityDictionary[$(this).attr('data-id')]);
       for (let key in amenityDictionary) {
-          if (i !== 0) {
-            result += ", " + amenityDictionary[key];
-          } else {
-             result += amenityDictionary[key];
-	  }
+        if (i !== 0) {
+          result += ', ' + amenityDictionary[key];
+        } else {
+          result += amenityDictionary[key];
+        }
         i += 1;
       }
-      $(".amenities h4").text(result);
+      $('.amenities h4').text(result);
     }
   });
 });
