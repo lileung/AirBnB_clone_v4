ad043f9ced7e0266e5c96f2888ed9668f2046e49
Lisa Leung <110@holbertonschool.com>
2017-09-30 22:22:34 +0000

    semistandard fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
+const $ = window.$;
+$(document).ready(function () {
   let amenityDictionary = {};
-  $("input").change(function(){
-    let result = "";
+  $('input').change(function () {
+    let result = '';
     let i = 0;
-    if(this.checked){
-      amenityDictionary[$(this).attr("data-id")] = $(this).attr("data-name");
+    if (this.checked) {
+      amenityDictionary[$(this).attr('data-id')] = $(this).attr('data-name');
       for (let key in amenityDictionary) {
         if (i !== 0) {
-          result += ", " + amenityDictionary[key];
+          result += ', ' + amenityDictionary[key];
         } else {
           result += amenityDictionary[key];
         }
         i += 1;
       }
-      $(".amenities h4").text(result);
-
-    }else {
-      let result = "";
+      $('.amenities h4').text(result);
+    } else {
+      let result = '';
       let i = 0;
-      delete (amenityDictionary[$(this).attr("data-id")]);
+      delete (amenityDictionary[$(this).attr('data-id')]);
       for (let key in amenityDictionary) {
-          if (i !== 0) {
-            result += ", " + amenityDictionary[key];
-          } else {
-             result += amenityDictionary[key];
-	  }
+        if (i !== 0) {
+          result += ', ' + amenityDictionary[key];
+        } else {
+          result += amenityDictionary[key];
+        }
         i += 1;
       }
-      $(".amenities h4").text(result);
+      $('.amenities h4').text(result);
     }
   });
 });
