0abdf27b0e954aae822250e3dc35ec0d03c59303
Lisa Leung <110@holbertonschool.com>
2017-09-30 21:30:42 +0000

    content populated via API

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	     let article = $('<article>');
 	     /*TITLE*/
 	     let title = $('<div/>', {class: 'title'}).appendTo(article); /*create title*/
-	     let h2title = $('<h2/>', {text:"hello title"}).appendTo(title); /*append to title*/
+	     let h2title = $('<h2/>', {text:data[i]['name']}).appendTo(title); /*append to title*/
              $('<div/>', { class: 'price_by_night', text: data[i].price_by_night}).appendTo(title);
 
 	     /*INFORMATION*/
@@ -23,26 +23,32 @@ $(document).ready(function(){
 	     /*GUEST ICON*/
 	     let guestIcon = $('<i/>', {class: 'fa fa-users fa-3x'}).appendTo(maxGuest);
 	     $('<br/>').appendTo(maxGuest);
-	     $('<div/>', {text: "5"}).appendTo(maxGuest);
+	     $('<div/>', {text: data[i]['max_guest'] + " Guests"}).appendTo(maxGuest);
 
 	     /*NUMBER OF ROOMS*/
              let roomIcon = $('<i/>', {class: 'fa fa-bed fa-3x'}).appendTo(numberRoom);
              $('<br/>').appendTo(numberRoom);
-             $('<div/>', {text: "5"}).appendTo(numberRoom);
+             $('<div/>', {text: data[i]['number_rooms'] + " Bedrooms"}).appendTo(numberRoom);
 
              /*NUMBER OF BATHS*/
              let bathIcon = $('<i/>', {class: 'fa fa-bath fa-3x'}).appendTo(numberBath);
              $('<br/>').appendTo(numberBath);
-             $('<div/>', {text: "5"}).appendTo(numberBath);
+             $('<div/>', {text: data[i]["number_bathrooms"] + " Bathroom"}).appendTo(numberBath);
 
              /*USER*/
              let user = $('<div/>', {class: 'user'});
-             $('<strong/>', {text : 'Owner: '}).appendTo(user);
-             user.appendTo(article);
 
-             /*DESCRIPTION*/
-	     let description = $('<div/>', {class: 'description', text: 'some description here'}).appendTo(user);
-
+	     /*Separate API call to populate user information*/
+	     $.ajax({
+                url: 'http://0.0.0.0:5001/api/v1/users/' + data[i]['user_id'],
+		type: 'get',
+		success: function(userData ){
+		  $('<strong/>', {text : 'Owner: ' + userData['first_name'] + " " + userData['last_name']}).appendTo(user);             
+		  user.appendTo(article);
+             let description = $('<div/>', {class: 'description', text: data[i]['description']}).appendTo(user);
+		  console.log(userData);
+		}
+	     });
 
 	     $('.places').append(article);
          }
