764f879cbfa832fc6cc50ba4f3e07a26e23f03a6
NamoDawn <naomisorrell@gmail.com>
2017-10-02 03:25:46 +0000

    reload places on search

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,61 +1,73 @@
 const $ = window.$;
 $(document).ready(function () {
   let amenityDictionary = {};
+  const userDictionary = {};
 
-  $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    type: 'post',
-    contentType: 'application/json',
-    data: JSON.stringify({}),
-    success: function (data) {
-      for (let i = 0; i < data.length; i++) {
-        let article = $('<article>');
-        /* TITLE */
-        let title = $('<div/>', {class: 'title'}).appendTo(article); /* create title */
-        $('<h2/>', {text: data[i]['name']}).appendTo(title); /* append to title */
-        $('<div/>', {class: 'price_by_night', text: '$' + data[i].price_by_night}).appendTo(title);
+  loadPlaces({});
 
-        /* INFORMATION */
-        let infoContainer = $('<div/>', {class: 'information'}).appendTo(article);
-        let maxGuest = $('<div/>', {class: 'max_guest'}).appendTo(infoContainer);
-        let numberRoom = $('<div/>', {class: 'number_rooms'}).appendTo(infoContainer);
-        let numberBath = $('<div/>', {class: 'number_bathrooms'}).appendTo(infoContainer);
-
-        /* GUEST ICON */
-        $('<i/>', {class: 'fa fa-users fa-3x'}).appendTo(maxGuest);
-        $('<br/>').appendTo(maxGuest);
-        $('<div/>', {text: data[i]['max_guest'] + ' Guests'}).appendTo(maxGuest);
-
-        /* NUMBER OF ROOMS */
-        $('<i/>', {class: 'fa fa-bed fa-3x'}).appendTo(numberRoom);
-        $('<br/>').appendTo(numberRoom);
-        $('<div/>', {text: data[i]['number_rooms'] + ' Bedrooms'}).appendTo(numberRoom);
-
-        /* NUMBER OF BATHS */
-        $('<i/>', {class: 'fa fa-bath fa-3x'}).appendTo(numberBath);
-        $('<br/>').appendTo(numberBath);
-        $('<div/>', {text: data[i]['number_bathrooms'] + ' Bathroom'}).appendTo(numberBath);
-
-        /* USER */
-        let user = $('<div/>', {class: 'user'});
-
-        /* Separate API call to populate user information */
-        $.ajax({
-          url: 'http://0.0.0.0:5001/api/v1/users/' + data[i]['user_id'],
-          type: 'get',
-          success: function (userData) {
-            $('<strong/>', {text: 'Owner: ' + userData['first_name'] + ' ' + userData['last_name']}).appendTo(user);
-            user.appendTo(article);
-            $('<div/>', {class: 'description', text: data[i]['description']}).appendTo(user);
-          }
-        });
-        $('.places').append(article);
+  function getUsers () {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/users/',
+      type: 'get',
+      success: function (userData) {
+        for (let i = 0; i < userData.length; i++) {
+          userDictionary[userData[i].id] = userData[i].first_name + ' ' + userData[i].last_name;
+        }
       }
-    },
-    error: function (jqXhr, textStatus, errorThrown) {
-      console.log(errorThrown);
-    }
-  });
+    });
+  }
+  function loadPlaces (placeList) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'post',
+      contentType: 'application/json',
+      data: JSON.stringify(placeList),
+      success: function (data) {
+        getUsers();
+        for (let i = 0; i < data.length; i++) {
+          let article = $('<article>');
+      /* TITLE */
+          let title = $('<div/>', {class: 'title'}).appendTo(article); /* create title */
+          $('<h2/>', {text: data[i]['name']}).appendTo(title); /* append to title */
+          $('<div/>', {class: 'price_by_night', text: '$' + data[i].price_by_night}).appendTo(title);
+
+    /* INFORMATION */
+          let infoContainer = $('<div/>', {class: 'information'}).appendTo(article);
+          let maxGuest = $('<div/>', {class: 'max_guest'}).appendTo(infoContainer);
+          let numberRoom = $('<div/>', {class: 'number_rooms'}).appendTo(infoContainer);
+          let numberBath = $('<div/>', {class: 'number_bathrooms'}).appendTo(infoContainer);
+
+    /* GUEST ICON */
+          $('<i/>', {class: 'fa fa-users fa-3x'}).appendTo(maxGuest);
+          $('<br/>').appendTo(maxGuest);
+          $('<div/>', {text: data[i]['max_guest'] + ' Guests'}).appendTo(maxGuest);
+
+    /* NUMBER OF ROOMS */
+          $('<i/>', {class: 'fa fa-bed fa-3x'}).appendTo(numberRoom);
+          $('<br/>').appendTo(numberRoom);
+          $('<div/>', {text: data[i]['number_rooms'] + ' Bedrooms'}).appendTo(numberRoom);
+
+    /* NUMBER OF BATHS */
+          $('<i/>', {class: 'fa fa-bath fa-3x'}).appendTo(numberBath);
+          $('<br/>').appendTo(numberBath);
+          $('<div/>', {text: data[i]['number_bathrooms'] + ' Bathroom'}).appendTo(numberBath);
+
+     /* USER */
+          let user = $('<div/>', {class: 'user'});
+
+    /* Separate API call to populate user information */
+          $('<strong/>', {text: 'Owner: ' + userDictionary[data[i]['user_id']]}).appendTo(user);
+          user.appendTo(article);
+          $('<div/>', {class: 'description', html: data[i]['description']}).appendTo(user);
+          $('.places').append(article);
+        }
+      },
+      error: function (jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    });
+  }
+
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
@@ -63,6 +75,7 @@ $(document).ready(function () {
       $('DIV#api_status').removeClass('available');
     }
   });
+
   $('input').change(function () {
     let result = '';
     let i = 0;
@@ -91,5 +104,10 @@ $(document).ready(function () {
       }
       $('.amenities h4').text(result);
     }
+  });
+
+  $('button').click(function () {
+    $('article').remove();
+    loadPlaces({'amenities': Object.keys(amenityDictionary)});
   });
 });
